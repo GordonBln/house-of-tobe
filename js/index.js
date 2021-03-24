f041cc700f9c868a7b4bb60e78e54e84280ce4f3
Okan Karakaya <okankarakaya@windowslive.com>
2021-03-24 20:01:26 +0100

    some changes

--- js/index.js
+++ js/index.js
@@ -2,13 +2,16 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 const slidedownMenu = document.querySelector(".slidedown-bar");
 const slidedownMenuIcon = document.querySelector(".hamburger-menu-slidedown");
+const body = document.querySelector("body");
 hamburgerMenu.addEventListener("click", () => {
   dropdownMenu.classList.toggle("active");
   slidedownMenuIcon.classList.toggle("active");
+  body.classList.toggle("overflow");
 });
 slidedownMenuIcon.addEventListener("click", () => {
   dropdownMenu.classList.toggle("active");
   slidedownMenuIcon.classList.toggle("active");
+  body.classList.toggle("overflow");
 });
 
 window.onscroll = () => {
